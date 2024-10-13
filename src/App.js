31a912aae5cab55e228b0a0287f5d98279b562d8
rime.laafia <rime.laafia@gmail.com>
2024-10-13 02:38:12 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-//import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PaymentPage from './pages/PaymentPage';
 import Header from './components/Header';
 import Footer from './components/Footer';
