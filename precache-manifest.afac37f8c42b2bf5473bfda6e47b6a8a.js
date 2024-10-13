d95781f4ae969a60a029ab8018ac1e52829abbb6
rime.laafia <rime.laafia@gmail.com>
2024-10-13 01:05:48 +0100

    Updates

---
+++ precache-manifest.afac37f8c42b2bf5473bfda6e47b6a8a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4760fc0fd086990d5d925b6794c7c365",
+    "url": "/SQE-Easy-Web-Dev/index.html"
+  },
+  {
+    "revision": "ff2958cd740828d0da88",
+    "url": "/SQE-Easy-Web-Dev/static/css/main.ebe60247.chunk.css"
+  },
+  {
+    "revision": "03f649f2a9fc9cb05444",
+    "url": "/SQE-Easy-Web-Dev/static/js/2.974c9436.chunk.js"
+  },
+  {
+    "revision": "7309e55c6cbe7349f1c6abcc59698d4e",
+    "url": "/SQE-Easy-Web-Dev/static/js/2.974c9436.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff2958cd740828d0da88",
+    "url": "/SQE-Easy-Web-Dev/static/js/main.0fb01353.chunk.js"
+  },
+  {
+    "revision": "2fabf620fa3fc020bdd7",
+    "url": "/SQE-Easy-Web-Dev/static/js/runtime-main.f9c55e1b.js"
+  },
+  {
+    "revision": "adb39db74bcf1f228f9f6c17b520634d",
+    "url": "/SQE-Easy-Web-Dev/static/media/logo.adb39db7.png"
+  },
+  {
+    "revision": "ce2f8711bf53ebc30ba0fec0937b6618",
+    "url": "/SQE-Easy-Web-Dev/static/media/mcq_questions.ce2f8711.csv"
+  },
+  {
+    "revision": "ce2f8711bf53ebc30ba0fec0937b6618",
+    "url": "/SQE-Easy-Web-Dev/static/media/mockexam1.ce2f8711.csv"
+  },
+  {
+    "revision": "940621188043b749888b57e99a7b443a",
+    "url": "/SQE-Easy-Web-Dev/static/media/questions.94062118.csv"
+  }
+]);
